947e496be4d245678040d80d48ae36f35d03e2f8
kavyatirumani <89557720+kavyatirumani@users.noreply.github.com>
2021-11-10 23:48:57 -0600

    n

--- models/Season.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require("mongoose") 
-const SeasonSchema = mongoose.Schema({ Season_type: String, temperature: Number, Season_month: String }) 
- 
-module.exports = mongoose.model("Season", SeasonSchema) 
--- routes/Season.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const Season_controlers= require('../controllers/Season'); 
-
-/* GET home page. */
-router.get('/', Season_controlers.Season_view_all_Page ); 
-
-module.exports = router;
